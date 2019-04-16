625732e24afd97d42918d19a019ab47c625966df
JenyZ <jenniferzirk@gmail.com>
2019-04-16 18:27:23 -0400

    fixed rotten tomatoes and readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# liri-node-app
+# Liri-node-app
+This app uses the OMDB Api to display  information for the movie title that the user inputs into node. 
+
+
+Before using this app you must clone it and create your own repository and add your own .env file with your personal API keys. You will also need to perform a npm install in your command line to install all of the node packages needed to run this Liri-node-app. 
+
+Then type in a movie title and enjoy all of the information it returns!
+
+# Author
+
+Jennifer Zirk
+
+# License
+
+Public Domain
+
+# Acknowledgements
+
+UCF Coding Bootcamp
+
+
+
--- liri.js
+++ liri.js
@@ -16,12 +16,16 @@ axios.get("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=" + keys
 
   function callBack(response) { 
 
-  var { Title, Year, Genre, imdbRating } = response.data;
-
-  console.log("The movie's rating is: " + imdbRating);
+  var { Title, Year, Genre, imdbRating, Ratings, Country, Language, Plot, Actors } = response.data;
   console.log("Title: ", Title);
+  console.log("The movie's IMDB rating is: " + imdbRating);
+  console.log("The movie's Rotten Tomatoes rating is: ", Ratings[1]);
   console.log("Year: ", Year);
-  console.log("Genre ", Genre);
+  console.log("Genre: ", Genre);
+  console.log("Country: ", Country);
+  console.log("Language: ", Language);
+  console.log("Plot: ", Plot);
+  console.log("Actors: ", Actors);
 
 };
 
